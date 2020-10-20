af7874624a81b14dd631b998a297b9133597b287
Jon Volden <volden.jon@gmail.com>
2020-10-19 19:39:36 -0500

    Initial commit.

---
+++ dstreams_of_anomalies/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for D(St)reams of Anomalies."""
+
+__author__ = """Jon Patrick Volden"""
+__email__ = 'volden@ku.edu'
+__version__ = '0.1.0'
---
+++ dstreams_of_anomalies/cli.py
@@ -0,0 +1,16 @@
+"""Console script for dstreams_of_anomalies."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for dstreams_of_anomalies."""
+    click.echo("Replace this message by putting your code into "
+               "dstreams_of_anomalies.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
